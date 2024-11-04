d86dd39c9a1dc3d0951b4da3278e138fbb184977
kartik0z <karthiksoni0007@gmail.com>
2024-11-04 10:28:59 +0530

    Create union.c

---
+++ C/C-Codes/union.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+union Data {
+    int i;
+    float f;
+    char str[20];
+};
+
+int main() {
+    union Data data;
+
+    data.i = 10;
+    printf("Data.i: %d\n", data.i);
+
+    data.f = 220.5;
+    printf("Data.f: %.2f\n", data.f);
+
+    strcpy(data.str, "Hello, Union!");
+    printf("Data.str: %s\n", data.str);
+
+    // Note: Printing data.i or data.f here would give unexpected results
+    // since data.str overwrites their memory space.
+
+    return 0;
+}
