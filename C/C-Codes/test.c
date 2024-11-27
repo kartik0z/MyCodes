ac3f7906c43a7e48784cf03a3f5d60e07f5e059e
kartik0z <karthiksoni0007@gmail.com>
2024-11-27 10:00:13 +0530

    Yes

--- C/C-Codes/test.c
+++ C/C-Codes/test.c
@@ -1,47 +1,31 @@
 #include <stdio.h>
 
-// Function to calculate factorial using recursion
-long long factorial(int n) {
-    if (n == 0 || n == 1) {
-        return 1;
-    }
-    return n * factorial(n - 1);
-}
-
-// Function to calculate Fibonacci term using recursion
-int fibonacci(int n) {
-    if (n == 0) {
-        return 0;
-    }
-    if (n == 1) {
-        return 1;
-    }
-    return fibonacci(n - 1) + fibonacci(n - 2);
-}
+struct Student {
+    char name[50];
+    int marks[5];
+};
 
 int main() {
-    int num, terms, i;
+    struct Student students[3];
+    int i, j;
 
-    // Factorial Calculation
-    printf("Enter a number to calculate its factorial: ");
-    scanf("%d", &num);
-
-    if (num < 0) {
-        printf("Factorial is not defined for negative numbers.\n");
-    } else {
-        printf("Factorial of %d is: %lld\n", num, factorial(num));
+    // Input details of students
+    for(i = 0; i < 3; i++) {
+        printf("Enter name of student %d: ", i + 1);
+        scanf("%s", students[i].name);
+        printf("Enter marks of 5 subjects for %s:\n", students[i].name);
+        for(j = 0; j < 5; j++) {
+            printf("Subject %d: ", j + 1);
+            scanf("%d", &students[i].marks[j]);
+        }
     }
 
-    // Fibonacci Series
-    printf("\nEnter the number of terms for the Fibonacci series: ");
-    scanf("%d", &terms);
-
-    if (terms <= 0) {
-        printf("Number of terms must be positive.\n");
-    } else {
-        printf("Fibonacci series up to %d terms:\n", terms);
-        for (i = 0; i < terms; i++) {
-            printf("%d ", fibonacci(i));
+    // Display details of students
+    for(i = 0; i < 3; i++) {
+        printf("\nStudent: %s\n", students[i].name);
+        printf("Marks: ");
+        for(j = 0; j < 5; j++) {
+            printf("%d ", students[i].marks[j]);
         }
         printf("\n");
     }
