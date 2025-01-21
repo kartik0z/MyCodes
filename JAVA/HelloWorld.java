83ae412e53575e3ede47d2b90ab0482e11e492f1
kartik0z <karthiksoni0007@gmail.com>
2025-01-21 18:49:40 +0530

    Compounding

--- JAVA/HelloWorld.java
+++ JAVA/HelloWorld.java
@@ -1,7 +1,30 @@
-// Save this file as HelloWorld.java
+import java.util.Scanner;
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("I love pizza");
+        Scanner scanner = new Scanner(System.in);
+
+        double principal;
+        double rate;
+        int timesCompounded;
+        int years;
+        double amount;
+
+        System.out.print("Enter the principal amount: ");
+        principal = scanner.nextDouble();
+
+        System.out.print("Enter the interest rate: ");
+        rate = scanner.nextDouble() / 100;
+        
+        System.out.print("Enter the # of times compounded per year: ");
+        timesCompounded = scanner.nextInt();
+
+        System.out.print("Enter the # of years: ");
+        years = scanner.nextInt();
+
+        amount = principal * Math.pow(1 + rate/timesCompounded, timesCompounded * years);
+        
+        System.out.printf("The amount after %d years is: %.2f", years, amount);
+
+        scanner.close();
     }
 }
