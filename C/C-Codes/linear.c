cf47fb6743d864cbcca00cfd722718169cc8df38
kartik0z <karthiksoni0007@gmail.com>
2025-01-31 12:25:54 +0530

    Update linear.c

--- C/C-Codes/linear.c
+++ C/C-Codes/linear.c
@@ -4,35 +4,30 @@
 int linearSearch(int arr[], int n, int key) {
     for(int i = 0; i < n; i++) {
         if(arr[i] == key) {
-            return i; // Return index if key is found
+            return i;
         }
     }
-    return -1; // Return -1 if key is not found
+    return -1;
 }
 
 int main() {
     int n, key;
 
-    // Input the number of elements in the array
     printf("Enter the number of elements: ");
     scanf("%d", &n);
 
     int arr[n];
 
-    // Input the elements of the array
     printf("Enter the elements of the array:\n");
     for(int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
-    // Input the key to search
     printf("Enter the element to search for: ");
     scanf("%d", &key);
 
-    // Perform linear search
     int result = linearSearch(arr, n, key);
 
-    // Output the result
     if(result != -1) {
         printf("Element found at index %d.\n", result);
     } else {
